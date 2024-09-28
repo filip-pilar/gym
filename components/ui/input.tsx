898018f7377fb57883358c0494f812463e4a5488
filip-pilar <filip.pilar@gmail.com>
2024-09-28 20:10:47 +0400

    pls

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
+// eslint-disable-next-line @typescript-eslint/no-empty-interface, @typescript-eslint/ban-types
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
