3a597d57d3c31620d02e8bb13e803ca7a8494e06
filip-pilar <filip.pilar@gmail.com>
2024-09-28 20:06:54 +0400

    reploy ffs

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,11 +2,8 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-
-// @ts-ignore
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
